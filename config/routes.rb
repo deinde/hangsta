97de827d9d5c7d5a04031c720e0f6dcc369f66f6
Charles Dickstein <charleswdickstein@gmail.com>
2015-06-01 17:53:11 -0400

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/create'
-
-  get 'users/update'
-
-  get 'users/edit'
-
-  get 'users/delete'
+  devise_for :users
+  root 'home#index'
+  
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
