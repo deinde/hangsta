250fc244b07614810e61173cca1905ba68912efc
Yi Ren <coderquestny@gmail.com>
2015-06-02 10:36:22 -0400

    merged from github account

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150602021650) do
+ActiveRecord::Schema.define(version: 20150602025725) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20150602021650) do
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "username"
-    t.binary   "avatar"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
