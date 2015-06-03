ff50e89c4461868c403f407e00a04c94a230eccf
deindedawodu <mr.dawodu@gmail.com>
2015-06-03 16:35:56 -0400

    'this is anotehr commit'

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20150603191815) do
   create_table "events", force: :cascade do |t|
     t.string   "title"
     t.string   "body"
-    t.string   "date"
+    t.datetime "date"
     t.integer  "user_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
