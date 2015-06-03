25180162fe26945d68b193714675b01e5c82ec30
Yi Ren <coderquestny@gmail.com>
2015-06-03 17:17:27 -0400

    ran rake migrate after merging. Deleted yeild from sessions#new page.

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20150603191815) do
   create_table "events", force: :cascade do |t|
     t.string   "title"
     t.string   "body"
-    t.datetime "date"
+    t.string   "date"
     t.integer  "user_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
