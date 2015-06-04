b4218d9506a42b469823ceb0d73aa8a49cfada11
deindedawodu <mr.dawodu@gmail.com>
2015-06-04 14:17:37 -0400

    'this button changes on index page'

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20150604161231) do
   create_table "events", force: :cascade do |t|
     t.string   "title"
     t.string   "body"
-    t.string   "date"
+    t.datetime "date"
     t.integer  "user_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
