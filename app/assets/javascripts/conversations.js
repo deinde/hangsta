7d91982ade28ace4756677b2fe88172983335aa6
deindedawodu <mr.dawodu@gmail.com>
2015-06-05 11:44:01 -0400

    hangsta logo float right

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -1,12 +1,12 @@
-var ready;
+// var ready;
  
-ready = function(){
-    // enable chosen js
-    $('.chosen-select').chosen({
-        no_results_text: 'No results matched'
-    });
-}
+// ready = function(){
+//     // enable chosen js
+//     $('.chosen-select').chosen({
+//         no_results_text: 'No results matched'
+//     });
+// }
  
-$(document).ready(ready);
-// if using turbolinks
-$(document).on("page:load",ready);
+// $(document).ready(ready);
+// // if using turbolinks
+// $(document).on("page:load",ready);
