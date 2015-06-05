518de116f2fba3f0738c7cdd00654b3584b2c8d1
Charles Dickstein <charleswdickstein@gmail.com>
2015-06-05 13:55:02 -0400

    map added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "default_pro_picture.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   has_many :events
+  validates :username, presence: true
 
 
     ######creating relationship for user and events######
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admins
   resources :events
+  # resources :places
 
   resources :relationships, only: [:create, :destroy]
 
